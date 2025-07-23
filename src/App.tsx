c9adf061f83cf39b859e1e3feb5eecab41f4185f
Bradley-Rice <bradley@ricefamily.co.za>
2025-07-23 11:44:02 +0200

    Began to fetch Data from  API

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import NavBar from "./Nav";
 import { Hero } from "./Hero";
+import FetchData from "./FetchData";
 
 function App(){
   return <div>
     <NavBar />
     <Hero />
   
+
+    <FetchData />
   </div>
 }
 
