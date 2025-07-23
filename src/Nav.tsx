d383958bc3e287c9fc40b1af4dd65d57c25dd11b
Bradley-Rice <bradley@ricefamily.co.za>
2025-07-23 09:31:47 +0200

    started with navigation menu

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,9 +1,33 @@
 // Navbar component.
-function NavBar(){
-    // Return the Navbar component.
-    return  <nav>
-                <h1>HELLO WORLD</h1>
-            </nav>
+
+export const NavBar = () => {
+  return (
+    <header className="bg-red-950">
+        <nav className="flex justify-between items-center w-[92%] mx-auto">
+            <div className="py-3">
+                <img className="w-16" src="./assets/images/nav-logo.png" alt="..." />
+            </div>
+            <div className="md:static absolute bg-red-950 md:min-h-fit min-h-[50vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5">
+                <ul className="flex md:flex-row flex-col md:items-center md:gap-4 gap-8">
+                    <li>
+                        <a className="hover:text-gray-500" href="">Home</a>
+                    </li>
+                    <li>
+                        <a className="hover:text-gray-500" href="">About</a>
+                    </li>
+                    <li>
+                        <a className="hover:text-gray-500" href="">Register</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="flex items-center gap-6">
+                <button className="bg-blue-600 text-white px-5 py-2 rounded-full hover:bg-green-500">Register</button>
+                <i className="fa-solid fa-bars text-3xl cursor-pointer md:invisible"></i>
+            </div>
+        </nav>
+    </header>
+    
+  )
 }
 
 export default NavBar;
